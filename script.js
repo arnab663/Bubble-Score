f77d41a24ac65ded2b90c8b6e1a801828a114d9b
arnab663 <am2524@cse.jgec.ac.in>
2024-06-29 18:53:51 +0530

    fixing an issue

--- script.js
+++ script.js
@@ -1,67 +1,4 @@
 
-// function makeBubble(){
-//     var clutter = ""
-
-//     for(var i=1; i<=160; i++){
-//         clutter+=`<div class="bubble">${Math.floor(Math.random()*10)}</div>`
-//     }
-
-//     document.querySelector('#panbottom').innerHTML = clutter
-// }
-
-// var timer = 30
-// function runtimer(){
-//     var timerInterval = setInterval(()=>{
-//         if(timer>0){
-//             timer--
-//             document.querySelector('#timerValue').textContent = timer
-//         }
-//         else{
-//             endOfGame()
-//             clearInterval(timerInterval)
-//         }
-//     },1000)
-// }
-
-
-// var score = 0
-// function increaseScore(hitVal){
-//     score+=hitVal
-//     document.querySelector('#scoreValue').textContent = score
-// }
-
-
-// var hit = 0
-// function hitCount(){
-//     hit++
-//     document.querySelector('#hitValue').textContent = hit
-// }
-
-
-// // Event bubbling 
-// document.querySelector('#panbottom').addEventListener('click',(dets)=>{
-//     // console.log(dets.target.className)
-//     if(dets.target.className=='bubble'){
-//         var hitBubbleValue = Number(dets.target.textContent)
-//         increaseScore(hitBubbleValue)
-//         hitCount()
-//         makeBubble()
-//     }
-// })
-
-// winModalStyle = "display:block;position: absolute;top: 20%;left: 30%;height: 40vh;width: 35vw;border-radius: 15px;background-color: #602c07;color: white;display: flex;flex-direction: column;gap: 15px;align-items: center;justify-content: center"
-
-// function endOfGame(){
-//     document.querySelector('#showScore').textContent = `Your score is ${score}`
-//     document.querySelector('#showHitCount').textContent = `You have made ${hit} hits`
-//     document.querySelector('#game-end').setAttribute("style",winModalStyle) 
-// }
-
-// runtimer()
-
-// makeBubble()
-
-
 var startBtn = document.querySelector("#start")
 startBtn.addEventListener('click',()=>{
     startBtn.setAttribute('style','display:none')
@@ -97,6 +34,7 @@ function startGame(){
     function increaseScore(hitVal){
         if(timer){
             score+=hitVal
+            console.log(score)
             document.querySelector('#scoreValue').textContent = score
         }
     }
@@ -137,6 +75,7 @@ function startGame(){
         document.querySelector('#showHitCount').textContent = `You have made ${hit} hits`
         document.querySelector('#game-end').setAttribute("style",winModalStyle) 
         document.querySelector('#panbottom').setAttribute('style','pointer-events:none')
+        document.querySelector('#restart').setAttribute('style','display:block')
     }
 
     runtimer()
